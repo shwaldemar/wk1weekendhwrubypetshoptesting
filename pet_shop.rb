1c2466ddc478609dcd9641605828437f2b5eba03
Simon <shwaldemar@gmail.com>
2019-05-17 13:34:06 +0100

    3rd sest works too

--- pet_shop.rb
+++ pet_shop.rb
@@ -4,11 +4,17 @@ return shopname[:name]
 end
 
 #2.
-  # def test_total_cash
-  #   sum = total_cash(@pet_shop)
-  #   assert_equal(1000, sum)
-  # end
+def total_cash(totalcash)
+  return totalcash[:admin][:total_cash]
+end
 
-  def total_cash(totalcash)
-    return totalcash[:admin][:total_cash]
-  end
+#3.
+# def test_add_or_remove_cash__add
+#   add_or_remove_cash(@pet_shop,10)
+#   cash = total_cash(@pet_shop)
+#   assert_equal(1010, cash)
+# end
+
+def add_or_remove_cash(add, tenner)
+  return add[:admin][:total_cash] += tenner
+end
--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -86,11 +86,11 @@ class TestPetShop < Minitest::Test
   end
 
   #3.
-  # def test_add_or_remove_cash__add
-  #   add_or_remove_cash(@pet_shop,10)
-  #   cash = total_cash(@pet_shop)
-  #   assert_equal(1010, cash)
-  # end
+  def test_add_or_remove_cash__add
+    add_or_remove_cash(@pet_shop,10)
+    cash = total_cash(@pet_shop)
+    assert_equal(1010, cash)
+  end
 
   #4.
   # def test_add_or_remove_cash__remove
